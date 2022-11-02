a391156aa05e815ceb4d6d5ad95f80ca5c2ed073
Chris Riccomini <criccomini@apache.org>
2022-11-02 16:37:31 -0700

    Extend records with helper methods

--- parser.js
+++ parser.js
@@ -4,10 +4,40 @@ const { parse } = require('csv-parse/sync');
 async function loadCsv(csvFilePath) {
   const content = await readFile(csvFilePath);
   const records = parse(content, {
-    cast: true,
-    cast_date: true
+    columns: true,
+    trim: true
   });
-  console.log(records);
+  return extendRecords(records);
+}
+
+function extendRecords(records) {
+  records.forEach((record) => {
+    record.choice = function(n) {
+      // n = choice number
+      // "Clay Owl"
+      return this[`Class choice #${n}`];
+    };
+    record.name = function() {
+      // Jane Gupta
+      return this['Your First and Last Name'];
+    };
+    record.absent = function() {
+      return this['If you know you will be absent on a Cluster Class day, please select the day(s) you will be absent'];
+    };
+    record.grade = function() {
+      // "4th - Wai"
+      return this['Your Grade/Teacher'].split(' - ')[0];
+    };
+    record.teacher = function() {
+      // "4th - Wai"
+      return this['Your Grade/Teacher'].split(' - ')[1];
+    };
+    record.timestamp = function() {
+      // "5/3/2022 9:33:49"
+      return this['Timestamp'];
+    }
+    console.log(record.name() + " " + record.timestamp() + " " + record.grade() + " " + record.teacher());
+  });
 }
 
 module.exports = {
