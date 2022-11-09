790a41baa06c312fc75723494930cf2b268ac4da
Chris Riccomini <criccomini@apache.org>
2022-11-09 09:43:56 -0800

    Look for 'day n' in absent column

--- preferences.js
+++ preferences.js
@@ -2,6 +2,8 @@ const { readFile } = require('fs/promises');
 const { parse } = require('csv-parse/sync');
 
 class Preference {
+  // choices = [<class 1 name>, <class 2 name>, ...]
+  // absents = [1, 3] (for someone missing day 1 and day 3)
   constructor(name, choices, absents, grade, teacher, timestamp) {
     this.name = name;
     this.choices = choices;
@@ -38,7 +40,13 @@ class Preferences {
         // choices. Get rid of these nulls.
         .filter(c => !!c);
       // TODO need to get these values set as day 1, day 2, etc in the spreadsheet
-      const absents = r['If you know you will be absent on a Cluster Class day, please select the day(s) you will be absent'];
+      const lowerCaseAbsentString = r['If you know you will be absent on a Cluster Class day, please select the day(s) you will be absent'].toLowerCase();
+      const absents =  Array.from(Array(8).keys())
+        // Try the first 8 days
+        .map(i => lowerCaseAbsentString.includes(`day ${i}`) ? 1 : null)
+        // There are never 8 days, so we're going to have some null days. Get
+        // rid of these nulls.
+        .filter(d => !!d);
       return new Preference(
         r['Your First and Last Name'],
         classes,
