6182a3d7749145fedb99a17b39a976d19c582f3e
Chris Riccomini <criccomini@apache.org>
2023-04-30 20:20:05 -0700

    Shuffle user choices if we can't assign them based on sorted order

--- assigner.js
+++ assigner.js
@@ -9,7 +9,10 @@ class Assigner {
   }
 
   assign() {
-    const [classAssignments, studentAssignments] = this.#assignByPopularity();
+    let [classAssignments, studentAssignments] = this.#assignByPopularity();
+    while (!this.#isFullyScheduled(studentAssignments)) {
+      [classAssignments, studentAssignments] = this.#assignByPopularity();
+    }
     log.info(classAssignments);
     log.info(studentAssignments);
     return Object.entries(studentAssignments).map(([studentName, days]) => {
@@ -46,11 +49,10 @@ class Assigner {
       for (let i = 0; classAssignments[className].length < clazz.minStudents; ++i) {
         const studentName = studentChoices[i][0];
         const studentAssignment = studentAssignments[studentName];
-        const preference = this.preferences.preferences[studentName];
 
         log.debug(`looking at "${studentName} for "${className}"`)
 
-        if (this.#isAssignmentAllowed(clazz, classAssignment, preference, studentAssignment)) {
+        if (this.#isAssignmentAllowed(clazz, classAssignment, studentAssignment)) {
           // Assign the student to the class.
           classAssignments[className].push(studentName);
           // Assign the class to the student for each day that it's held.
@@ -66,31 +68,66 @@ class Assigner {
     log.debug('assigning by choice');
     // Assign remaining students that aren't yet fully assigned.
     Object.entries(this.preferences.preferences).forEach(([studentName, preference]) => {
-      for (let i = 0; i < preference.choices.length && Object.keys(studentAssignments[studentName]).length < this.schedule.numDays(); ++i) {
-        const studentAssignment = studentAssignments[studentName];
-        const className = preference.choices[i];
-        const classAssignment = classAssignments[className];
-        const clazz = this.schedule.classes[className];
+      // Try to fully assign the student. First pass, use their sorted choices. 
+      // If sorted choices don't work, shuffle the choices and try again. Do
+      // this until the student is fully assigned (or we've tried 999 times).
+      for (let tries = 0; tries < 1000 && !this.#isFullyAssigned(studentAssignments, studentName); ++tries) {
+        // Clone the student's choices so we can shuffle then if we need to.
+        const shuffledChoices = [...preference.choices];
+        if (tries > 0) {
+          // If we weren't able to assign the student based on their choice order,
+          // then shuffle their choices and try again.
+          shuffledChoices.sort(() => Math.random() - 0.5);
+          // Reset the student's assignments since we're trying to assign again.
+          this.#clearStudent(classAssignments, studentAssignments, studentName);
+        }
+        for (let i = 0; i < shuffledChoices.length && !this.#isFullyAssigned(studentAssignments, studentName); ++i) {
+          const studentAssignment = studentAssignments[studentName];
+          const className = shuffledChoices[i];
+          const classAssignment = classAssignments[className];
+          const clazz = this.schedule.classes[className];
 
-        log.debug(`looking at "${studentName} for "${className}"`)
+          log.debug(`looking at "${studentName} for "${className}"`)
 
-        if (this.#isAssignmentAllowed(clazz, classAssignment, preference, studentAssignment)) {
-          // Assign the student to the class.
-          classAssignments[className].push(studentName);
-          // Assign the class to the student for each day that it's held.
-          clazz.days.forEach(d => {
-            studentAssignments[studentName][`Day ${d}`] = clazz.name;
-          });
+          if (this.#isAssignmentAllowed(clazz, classAssignment, studentAssignment)) {
+            // Assign the student to the class.
+            classAssignments[className].push(studentName);
+            // Assign the class to the student for each day that it's held.
+            clazz.days.forEach(d => {
+              studentAssignments[studentName][`Day ${d}`] = clazz.name;
+            });
+          }
         }
       }
     });
+  }
+
+  #clearStudent(classAssignments, studentAssignments, studentName) {
+    for (const className of Object.values(studentAssignments[studentName])) {
+      var index = classAssignments[className].indexOf(studentName);
+      classAssignments[className].splice(index, 1);
+    }
+    studentAssignments[studentName] = {}
+  }
+
+  #isFullyAssigned(studentAssignments, studentName) {
+    return Object.keys(studentAssignments[studentName]).length >= this.schedule.numDays();
+  }
+
+  #isFullyScheduled(studentAssignments) {
+    for (const studentName of Object.keys(this.preferences.preferences)) {
+      if (!this.#isFullyAssigned(studentAssignments, studentName)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   #assignRemaining(classAssignments, studentAssignments) {
     log.debug('assigning remaining');
     // Assign remaining students that aren't yet fully assigned.
     Object.entries(this.preferences.preferences).forEach(([studentName, preference]) => {
-      for (let i = 0; i < this.classes.length && Object.keys(studentAssignments[studentName]).length < this.schedule.numDays(); ++i) {
+      for (let i = 0; i < this.classes.length && !this.#isFullyAssigned(studentAssignments, studentName); ++i) {
         const studentAssignment = studentAssignments[studentName];
         const className = this.classes[i];
         const classAssignment = classAssignments[className];
@@ -141,7 +178,7 @@ class Assigner {
     return studentChoicesByClass;
   }
 
-  #isAssignmentAllowed(clazz, classAssignment, preference, studentAssignment) {
+  #isAssignmentAllowed(clazz, classAssignment, studentAssignment) {
     const classIsNotFull = classAssignment.length < clazz.maxStudents;
     const studentNotBookedOnClassDays = clazz
       .days
